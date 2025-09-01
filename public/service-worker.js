50c16f7857349e76bbcfd5c5b9f68d90b66d63c2
Subhash <subashyagantisubbu@gmail.com>
2025-09-01 21:15:06 +0530

    index.html and manifest.json

---
+++ public/service-worker.js
@@ -0,0 +1,24 @@
+// public/service-worker.js
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("portfolio-cache").then((cache) => {
+      return cache.addAll([
+        "/",            // your homepage
+        "/index.html",  // main file
+        "/manifest.json",
+        "/favicon.ico",
+        "/logo192.png", // icons (replace with yours)
+        "/logo512.png",
+      ]);
+    })
+  );
+  console.log("Service Worker installed!");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
